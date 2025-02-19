7ef2eee57f8cc14b6f1901e1d7d876f0f1946013
jeyarni <jeya08sivam@gmail.com>
2025-02-19 15:11:46 +0530

    next

---
+++ nextjs-with-mosh-13.02.2025/app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <div> <button onClick={()=>console.log("Clicked")}>Add to Cart</button></div>
+  )
+}
+
+export default AddToCard
